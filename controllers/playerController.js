78b23808f615c574d2cc66b01802ca5701630e68
Valentine <valentinemaillard1@gmail.com>
2020-07-08 13:45:11 -0400

    changed pagination limit to 50

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -67,7 +67,7 @@ exports.getAllPlayers = async (req, res) => {
 
         // PAGINATION
         const page = parseInt(req.query.page) || 1;
-        const limit = 100;
+        const limit = 50;
         const skip = (page - 1) * limit;
 
         query = query.skip(skip).limit(limit);
